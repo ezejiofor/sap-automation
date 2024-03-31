6be0657d0a6a55d517f1cfa7285b653ebfb21ca7
George Ezejiofor <okwuchukwu.ezejiofor.admin@gissandbox.onmicrosoft.com>
2024-03-31 20:54:37 +0100

    Updating sap automation repo path parameter on samples folder

--- deploy/scripts/deploy_controlplane.sh
+++ deploy/scripts/deploy_controlplane.sh
@@ -273,6 +273,8 @@ if [ 0 == $step ]; then
         rm -Rf .terraform terraform.tfstate*
     fi
 
+    sudo chmod +x "${SAP_AUTOMATION_REPO_PATH}"/deploy/scripts/install_deployer.sh
+
     "${SAP_AUTOMATION_REPO_PATH}"/deploy/scripts/install_deployer.sh $allParams
     return_code=$?
     if [ 0 != $return_code ]; then
