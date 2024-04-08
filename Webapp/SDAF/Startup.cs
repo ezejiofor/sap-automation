980f40462b6199c9c7da2bfce9094451de555d69
George Ezejiofor <okwuchukwu.ezejiofor.admin@gissandbox.onmicrosoft.com>
2024-04-08 20:06:31 +0100

    FIX: Updated ASPNETCORE_ENVIRONMENT to Production

--- Webapp/SDAF/Startup.cs
+++ Webapp/SDAF/Startup.cs
@@ -56,14 +56,14 @@ namespace AutomationForm
     {
       if (env.IsDevelopment())
       {
-        app.UseDeveloperExceptionPage();
+          app.UseDeveloperExceptionPage();
       }
       else
       {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
+          app.UseExceptionHandler("/Home/Error");
+          app.UseHsts();
       }
+
       app.UseHttpsRedirection();
       app.UseStaticFiles();
 
@@ -71,11 +71,12 @@ namespace AutomationForm
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
-        endpoints.MapRazorPages();
+          endpoints.MapControllerRoute(
+              name: "default",
+              pattern: "{controller=Home}/{action=Index}/{id?}");
+          endpoints.MapRazorPages();
       });
     }
   }
 }
+
